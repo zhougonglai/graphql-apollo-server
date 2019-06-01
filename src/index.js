d44d7cfc54f58795e7639edcdf594dfa2400d4b1
周公来 <zhougonglai@dingtalk.com>
2019-06-01 21:37:24 +0800

    type: rebuild

--- src/index.js
+++ src/index.js
@@ -62,17 +62,17 @@ server.applyMiddleware({
 
 connectDB(process.env.MONGODB_URI)
   .then(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      // app.listen({ port: 4000 }, () => {
-      //   console.log(`🚀 Server ready at ${server.graphqlPath}`);
-      // });
-      const httpServer = http.createServer(app);
-      server.installSubscriptionHandlers(httpServer);
-      httpServer.listen({ port: process.env.PORT || 4000 }, () => {
-        console.log(`🚀 Server ready at ${server.graphqlPath}`);
-        console.log(`🚀 Subscriptions ready at ${server.subscriptionsPath}`);
-      });
-    }
+    // if (process.env.NODE_ENV !== 'production') {
+    // app.listen({ port: 4000 }, () => {
+    //   console.log(`🚀 Server ready at ${server.graphqlPath}`);
+    // });
+    const httpServer = http.createServer(app);
+    server.installSubscriptionHandlers(httpServer);
+    httpServer.listen({ port: process.env.PORT || 4000 }, () => {
+      console.log(`🚀 Server ready at ${server.graphqlPath}`);
+      console.log(`🚀 Subscriptions ready at ${server.subscriptionsPath}`);
+    });
+    // }
   })
 
 // module.exports = app;
